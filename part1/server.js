71a1b918fc4a5e23dc8a0e195d2d220900b87d25
Marcus Ames <marcusames00@gmail.com>
2018-10-25 14:52:50 -0700

    Finished Challenges and Streches

--- part1/server.js
+++ part1/server.js
@@ -1,6 +1,27 @@
 var express = require('express');
 var app = express();
+const fs = require('fs')
+const path = require('path')
 var port = process.env.PORT || 8000;
+
+app.get('/hello', (req, res) => res.send('Hello!'));
+
+app.post('/create/:name', (req, res) => {
+  let names = { id: 1, name: req.params.name }
+  res.json(names)
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname+'/index.html'))
+})
+
+app.get('/verify/:age', (req, res) => {
+  if(req.params.age > 13) {
+    res.sendStatus(200)
+  } else {
+    res.sendStatus(403)
+  }
+})
 
 app.get('/yourroute', function(req, res) {
   res.send("stuff");
--- part2/server.js
+++ part2/server.js
@@ -1,6 +1,44 @@
 var express = require('express');
 var app = express();
+const fs = require('fs')
 var port = process.env.PORT || 8000;
+
+app.post('/create/:name/:age', (req, res) => {
+  let id;
+  let data = JSON.parse(fs.readFileSync('./storage.json', 'utf8'));
+  if(data.people.length > 0) {
+    id = data.people[data.people.length-1].id + 1;
+  } else {
+    id = 1
+  }
+  let obj = {
+    id,
+    name: req.params.name,
+    age: req.params.age
+  }
+  res.json(obj)
+  data.people.push(obj)
+  fs.writeFileSync('./storage.json', JSON.stringify(data));
+})
+
+app.get('/:id', (req, res) => {
+  let foundObj;
+  let data = JSON.parse(fs.readFileSync('./storage.json', 'utf8'))
+  for(let i = 0; i < data.people.length; i++) {
+    if(data.people[i].id === Number(req.params.id)){
+      foundObj = data.people[i]
+    }
+  }
+  if(foundObj){
+    res.json(foundObj)
+  } else {
+    res.sendStatus(400)
+  }
+})
+
+app.get('/', (req, res) => {
+  res.json(JSON.parse(fs.readFileSync('./storage.json', 'utf8')))
+})
 
 app.get('/yourroute', function(req, res) {
   res.send("stuff");
